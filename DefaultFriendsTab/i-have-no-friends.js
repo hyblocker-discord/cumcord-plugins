2029dd394b33eaba66b85d3ea972dee46a59f423
Hyblocker <bryan.spiteri@gmail.com>
2021-10-02 23:21:12 +0200

    maybe fix default friends tab

--- DefaultFriendsTab/i-have-no-friends.js
+++ DefaultFriendsTab/i-have-no-friends.js
@@ -2,49 +2,54 @@ import { log } from "@cumcord/utils/logger";
 import { findInReactTree } from "@cumcord/utils";
 import { after } from "@cumcord/patcher";
 import { webpack } from "@cumcord/modules";
-import { React } from "@cumcord/modules/common";
+import { React, i18n } from "@cumcord/modules/common";
 import Settings from "./Settings";
 
 let injections = [];
 
 export default ({ persist }) => {
-  const TabBar = webpack.findByDisplayName("TabBar");
+    const TabBar = webpack.findByDisplayName("TabBar");
 
-  async function waitFor (selector) {
-    const item = document.querySelectorAll(selector)[0];
-    if (!item)
-      return new Promise(window.requestAnimationFrame(waitFor(selector)));
-    return new Promise(item);
-  }
+    const sleep = (time) =>
+        new Promise(resolve =>
+            setTimeout(resolve, time)
+        );
+    const waitFor = async (querySelector) => {
+        let elem;
+        while (!(elem = document.querySelector(querySelector))) await sleep(1);
+        return elem;
+    };
 
-  return {
-    onLoad() {
-      injections.push(
-        after("render", TabBar.prototype, (_, res) => {
-          
-          let defaultTab = 'ONLINE';
-          defaultTab = persist.ghost.tab === undefined ? defaultTab : persist.ghost.tab ;
+    return {
+        onLoad() {
+            injections.push(
+                after("render", TabBar.prototype, (_, res) => {
 
-          (async () => {
-            // Wait for selected online element
-            await waitFor(`.tabBar-ZmDY9v.topPill-30KHOu > .item-3HknzM.selected-3s45Ha[data-item-id="ONLINE"]`);
+                    if (res.props['aria-label'] === i18n.Messages.FRIENDS) {
+                        let defaultTab = 'ONLINE';
+                        defaultTab = persist.ghost.tab === undefined ? defaultTab : persist.ghost.tab;
 
-            // Select our tab instead
-            res._owner.memoizedProps.onItemSelect(defaultTab);
-          })();
+                        (async () => {
+                          // Wait for selected online element
+                          await waitFor(`.tabBar-ZmDY9v.topPill-30KHOu > .item-3HknzM.selected-3s45Ha[data-item-id="ONLINE"]`);
 
-          return res;
-        })
-      );
-    },
+                          // Select our tab instead
+                          res._owner.memoizedProps.onItemSelect(defaultTab);
+                        })();
 
-    onUnload() {
-      for (let i = 0; i < injections.length; i++) {
-        injections[i]();
-      }
-      injections.length = 0;
-    },
+                        return res;
+                    }
+                })
+            );
+        },
 
-    settings: React.createElement(Settings, { persist }),
-  };
+        onUnload() {
+            for (let i = 0; i < injections.length; i++) {
+                injections[i]();
+            }
+            injections.length = 0;
+        },
+
+        settings: React.createElement(Settings, { persist }),
+    };
 };
